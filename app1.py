d959a0e679049af9523181a3ee1cf40c15ffe558
Sabeeh Rehman <64120439+rehmansabeeh@users.noreply.github.com>
2020-07-27 23:20:38 +0500

    fixed bug

--- app1.py
+++ app1.py
@@ -14,7 +14,6 @@ from flask_pymongo import PyMongo
 import bcrypt
 
 
-
 app = Flask(__name__)
 client = MongoClient("localhost", 27017)
 db = client['QawaidForDyslexiaDB']
@@ -24,26 +23,27 @@ app.config['MONGO_URI'] = "mongodb://localhost:27017/QawaidForDyslexiaDB"
 
 mongo = PyMongo(app)
 
+
 @app.route("/",  methods=['GET', 'POST'])
 def index():
     return render_template("chillaid/pages/basic-grid.html")
 
 
-@app.route('/result',methods = ['POST', 'GET'])
+@app.route('/result', methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
         text = request.form.get("text")
-        text_dict= {'textstring': text}
-        collection_user.insert_one(text_dict) 
+        text_dict = {'textstring': text}
+        collection_user.insert_one(text_dict)
         urdu_documents = collection_urdu_dictionary.find()
         user_documents = collection_user.find()
-    
+
     urdu_response = []
     for document in urdu_documents:
         document['_id'] = str(document['_id'])
         document['correctword'] = str(document['correctword'])
         document['form1'] = str(document['form1'])
-        
+
         urdu_response.append(document)
 
     print("urdu response", urdu_response)
@@ -55,7 +55,7 @@ def result():
     print("response", response)
 
     user_latest_text_dict = response[-1]
-    user_text_string = user_latest_text_dict['textstring']    
+    user_text_string = user_latest_text_dict['textstring']
     user_text_tokens = word_tokenize(user_text_string)
     final_results = []
     print(user_text_tokens)
@@ -64,14 +64,15 @@ def result():
         count = 0
         for document in urdu_response:
             if element == document['correctword']:
-                count = count + 1  
-        if count >0:     
-            final_results.append({"user_input": element, "found_count_in_db": count,"status": True,"user_input_length" : len(element), "suggested_word": element, "comments": "", "edit_distance":0, "found_count_in_worforms": 0})      
-        else: 
-            final_results.append({"user_input": element, "found_count_in_db": count,"status": False,"user_input_length" : len(element), "suggested_word": "", "comments": "",  "edit_distance":-1, "found_count_in_worforms": 0})
+                count = count + 1
+        if count > 0:
+            final_results.append({"user_input": element, "found_count_in_db": count, "status": True, "user_input_length": len(
+                element), "suggested_word": element, "comments": "", "edit_distance": 0, "found_count_in_worforms": 0})
+        else:
+            final_results.append({"user_input": element, "found_count_in_db": count, "status": False, "user_input_length": len(
+                element), "suggested_word": "", "comments": "",  "edit_distance": -1, "found_count_in_worforms": 0})
 
-    # initial_results = []    
-    
+    # initial_results = []
 
     for element in final_results:
         for key in element:
@@ -86,7 +87,6 @@ def result():
                                 if urdu_doc[urdu_key] == element[key]:
                                     element["comments"] = "It is probably a word"
                                     element["suggested_word"] = urdu_doc["correctword"]
-                                    
 
     for element in final_results:
         min_ed = element["edit_distance"]
@@ -94,36 +94,41 @@ def result():
             for urdu_doc in urdu_response:
                 for urdu_key in urdu_doc:
                     if not (urdu_key == "_id"):
-                            #print("key", key, "urdu key",urdu_doc[urdu_key])
-                            med = editDistance(element["user_input"], urdu_doc[urdu_key], len(element["user_input"]), len(urdu_doc[urdu_key]) )
-                            
-                            if min_ed == -1:
-                                min_ed = med
-                            if med < min_ed:
-                                min_ed = med
+                        #print("key", key, "urdu key",urdu_doc[urdu_key])
+                        med = editDistance(element["user_input"], urdu_doc[urdu_key], len(
+                            element["user_input"]), len(urdu_doc[urdu_key]))
+
+                        if min_ed == -1:
+                            min_ed = med
+                        if med < min_ed:
+                            min_ed = med
 
                     element["edit_distance"] = min_ed
 
     s = ""
     for element in final_results:
         if element["status"] == True:
-            s = s +"<span style='display:inline; color:green'>"+ element["user_input"]+"</span>,"
+            s = s + "<span style='display:inline; color:green'>" + \
+                element["user_input"]+"</span>,"
         else:
-            s = s +"<span style='display:inline; color:red'>"+ element["user_input"]+"</span>,"
-
+            s = s + "<span style='display:inline; color:red'>" + \
+                element["user_input"]+"</span>,"
 
     suggested_word_input = ""
     for element in final_results:
-        suggested_word_input = suggested_word_input +"<span style='display:inline; color:black'>"+ element["suggested_word"]+"</span>,"
+        suggested_word_input = suggested_word_input + \
+            "<span style='display:inline; color:black'>" + \
+            element["suggested_word"]+"</span>,"
 
     print(suggested_word_input)
-    return render_template("index-u.html", f=s , correct= suggested_word_input, spellCount= 3, grammerCount = 1, sentenceStructure= 1)
+    return render_template("index-u.html", f=s, correct=suggested_word_input, spellCount=3, grammerCount=1, sentenceStructure=1)
     # return render_template("chillaid/pages/basic-grid.html")
+
 
 @app.route("/login", methods=['GET'])
 def login():
-    # return render_template('login-user.html')
-    return render_template('login-u.html')
+    return render_template('login-user.html')
+    # return render_template('login-u.html')
 
 
 @app.route("/login-verification", methods=['POST', 'GET'])
@@ -131,7 +136,7 @@ def loginverification():
     if request.method == 'POST':
         users = mongo.db['users']
         print(users)
-        login_user = users.find_one({'username' : request.form['uname']})
+        login_user = users.find_one({'username': request.form['uname']})
         print(login_user)
         print("hello")
         if login_user:
@@ -139,42 +144,43 @@ def loginverification():
             print(request.form['psw'])
             print(request.form['psw'].encode('UTF-8'))
             print(login_user['password'])
-            print(bcrypt.hashpw(request.form['psw'].encode('UTF-8'), login_user['password']))
+            print(bcrypt.hashpw(request.form['psw'].encode(
+                'UTF-8'), login_user['password']))
             if bcrypt.hashpw(request.form['psw'].encode('UTF-8'), login_user['password']) == login_user['password']:
                 print("yuaba daba doooo")
                 session['username'] = request.form['uname']
-                
+
                 print("hello3")
                 return redirect(url_for('taketest'))
 
         return 'Invalid username/password combination'
 
 
-@app.route("/register", methods = ['GET'])
+@app.route("/register", methods=['GET'])
 def register():
     # return render_template('register-user.html')
     return render_template('register-u.html')
-
 
 
 @app.route('/registration', methods=['POST', 'GET'])
 def registerationprocess():
     if request.method == 'POST':
         users = mongo.db['users']
-        existing_user = users.find_one({'username' : request.form['uname']})
+        existing_user = users.find_one({'username': request.form['uname']})
         print(existing_user)
         if existing_user is None:
             print("hello4")
-            
-            hashpass = bcrypt.hashpw(request.form['psw'].encode('utf-8'), bcrypt.gensalt())
-            
+
+            hashpass = bcrypt.hashpw(
+                request.form['psw'].encode('utf-8'), bcrypt.gensalt())
+
             users.insert_one(
                 {
-                    'username' : request.form['uname'],
-                    'password' : hashpass,
-                    'isDyslexic' : request.form['isDyslexic'],
+                    'username': request.form['uname'],
+                    'password': hashpass,
+                    'isDyslexic': request.form['isDyslexic'],
                     'language': request.form['language'],
-                    'reading': request.form['reading'] 
+                    'reading': request.form['reading']
 
                 }
             )
@@ -188,14 +194,13 @@ def registerationprocess():
     return render_template("chillaid/testList.html")
 
 
-@app.route("/taketest", methods = ['GET','POST'])
+@app.route("/taketest", methods=['GET', 'POST'])
 def taketest():
     # return render_template("taketest.html")
     return render_template("chillaid/testList.html")
 
 
-
-@app.route("/test1", methods = ['GET', 'POST'])
+@app.route("/test1", methods=['GET', 'POST'])
 def test1():
     audios = os.listdir('static/audio/')
     collection_test = db['Test']
@@ -208,12 +213,11 @@ def test1():
     audio2 = audios[2]
     audio3 = audios[3]
 
-
     # return render_template("test1.html", audio0 = audio0, audio1 = audio1, audio2 = audio2, audio3 = audio3, audio4= audio4)
-    return render_template("chillaid/test1page.html", audio0 = audio0, audio1 = audio1, audio2 = audio2, audio3 = audio3)
+    return render_template("chillaid/test1page.html", audio0=audio0, audio1=audio1, audio2=audio2, audio3=audio3)
 
 
-@app.route('/test1page2',methods = ['GET', 'POST'])
+@app.route('/test1page2', methods=['GET', 'POST'])
 def test1page2():
 
     ans1 = request.form['question1']
@@ -224,7 +228,6 @@ def test1page2():
     print("ans2",  ans2)
     print("ans3",  ans3)
     print("ans4",  ans4)
-    
 
     imgNames = os.listdir('static/imgs')
     img0 = imgNames[0]
@@ -234,10 +237,10 @@ def test1page2():
     img4 = imgNames[4]
     print(imgNames)
     # return render_template("test1page2.html", img0 = img0,img1 = img1, img2 = img2, img3 = img3, img4= img4)
-    return render_template("chillaid/testImage.html", img0 = img0,img1 = img1, img2 = img2, img3 = img3, img4= img4)
+    return render_template("chillaid/testImage.html", img0=img0, img1=img1, img2=img2, img3=img3, img4=img4)
 
 
-@app.route("/test1finalpage", methods = ['GET', 'POST'])
+@app.route("/test1finalpage", methods=['GET', 'POST'])
 def test1finalpage():
     ans1 = request.form['question1']
     ans2 = request.form['question2']
@@ -246,47 +249,52 @@ def test1finalpage():
     ans5 = request.form['question5']
     return render_template("test1finalpage.html")
 
-@app.route("/test2", methods = ['GET', 'POST'])
+
+@app.route("/test2", methods=['GET', 'POST'])
 def test2():
     return render_template("test2.html")
 
-@app.route("/test3", methods = ['GET', 'POST'])
+
+@app.route("/test3", methods=['GET', 'POST'])
 def test3():
     return render_template("test3.html")
 
-@app.route("/test4", methods = ['GET', 'POST'])
+
+@app.route("/test4", methods=['GET', 'POST'])
 def test4():
     return render_template("test4.html")
 
-@app.route("/test5", methods = ['GET', 'POST'])
+
+@app.route("/test5", methods=['GET', 'POST'])
 def test5():
     return render_template("test5.html")
 
-@app.route("/test6", methods = ['GET', 'POST'])
+
+@app.route("/test6", methods=['GET', 'POST'])
 def test6():
     return render_template("test6.html")
 
-@app.route("/test7", methods = ['GET', 'POST'])
+
+@app.route("/test7", methods=['GET', 'POST'])
 def test7():
     return render_template("test7.html")
 
-@app.route("/test8", methods = ['GET', 'POST'])
+
+@app.route("/test8", methods=['GET', 'POST'])
 def test8():
     return render_template("test8.html")
 
-@app.route("/test9", methods = ['GET', 'POST'])
+
+@app.route("/test9", methods=['GET', 'POST'])
 def test9():
     return render_template("test9.html")
 
-@app.route("/test10", methods = ['GET', 'POST'])
+
+@app.route("/test10", methods=['GET', 'POST'])
 def test10():
     return render_template("test10.html")
-
-
 
 
 if __name__ == "__main__":
     app.secret_key = 'mysecret'
     app.run()
-
-
